932718ca97a83a1f5f0550d0775fa6c536500e75
victoria <victoria@limenius.com>
2016-04-17 22:39:29 +0200

    Two more levels

--- src/levels/index.js
+++ src/levels/index.js
@@ -12,13 +12,15 @@ const levels = [
         phases: [
             {
                 time: 5,
+                // square
                 tool: [[0, 0], [0, 50], [50, 50], [50, 0]],
                 center: [25, 25],
                 type: 'add',
                 color: colors.highlight,
             },
             {
-                time: 7,
+                time: 5,
+                // square
                 tool: [[0, 0], [0, 50], [50, 50], [50, 0]],
                 center: [25, 25],
                 type: 'subtract',
@@ -37,6 +39,7 @@ const levels = [
         phases: [
             {
                 time: 5,
+                // triangle up
                 tool: [[50, 0], [0, 50], [100, 50]],
                 center: [50, 25],
                 type: 'add',
@@ -44,6 +47,7 @@ const levels = [
             },
             {
                 time: 5,
+                // triangle up
                 tool: [[50, 0], [0, 50], [100, 50]],
                 center: [25, 25],
                 type: 'subtract',
@@ -51,6 +55,61 @@ const levels = [
             },
         ],
         goal: [[0, 50], [0, 150], [50, 100], [100, 150], [100, 50], [50, 0]],
+
+    },
+    {
+        title: 'Third level',
+        initialShape: {
+            points: [[0, 0], [0, 100], [100, 100], [100, 0]],
+            position: [100, 100]
+        },
+        phases: [
+            {
+                time: 5,
+                // triangle down
+                tool: [[0, 0], [50, 50], [100, 0]],
+                center: [50, 25],
+                type: 'add',
+                color: colors.highlight,
+            },
+            {
+                time: 5,
+                // square
+                tool: [[0, 0], [0, 50], [50, 50], [50, 0]],
+                center: [25, 25],
+                type: 'subtract',
+                color: colors.tertiary,
+            },
+        ],
+        goal: [[0, 0], [0, 100], [50, 150], [100, 100], [100, 50], [50, 50], [50, 0]],
+
+    },
+    {
+        title: 'Fourth level',
+        initialShape: {
+            points: [[0, 0], [0, 100], [100, 100], [100, 0]],
+            position: [100, 100]
+        },
+        phases: [
+            {
+                time: 5,
+                // half triangle down right
+                tool: [[0, 0], [0, 50], [50, 0]],
+                center: [25, 25],
+                type: 'subtract',
+                color: colors.highlight,
+            },
+            {
+                time: 5,
+                // half triangle down left
+                tool: [[0, 0], [50, 50], [50, 0]],
+                center: [25, 25],
+                type: 'add',
+                color: colors.tertiary,
+            },
+        ],
+        goal: [[0, 50], [50, 50], [0, 100], [50, 100], [100, 150], [100, 0], [50, 0]],
+        //goal: [[0, 50], [0, 100], [50, 100], [100, 150], [100, 75], [50, 75], [100, 25], [100, 0], [50, 0]],
 
     }
 ]
