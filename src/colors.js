014156709a6b15a816d02eb4c4d954e8f88a82ab
nacho <nacho@limenius.com>
2016-04-19 00:21:12 +0200

    Color choice

--- src/colors.js
+++ src/colors.js
@@ -1,15 +1,17 @@
 const colors = {
+    //background: 0xFFFF8000,
+    //primary: 0xFFA64D,
+    //secondary: 0xFFB366,
+    //tertiary: 0xFFC080,
+    //highlight: 0xFFcc99,
+    //border: 0xFFC9B3,
+    //
     background: 0x22222222,
-    //primary: 'hsla(0, 85%, 60%, 1)',
-    //secondary: 'hsla(32, 85%, 60%, 1)',
-    //tertiary: 'hsla(64, 85%, 60%, 1)',
-    //highlight: 'hsla(96, 85%, 60%, 1)',
-    //border: 'hsla(96, 85%, 60%, 1)',
-        primary: 0xFF57467B,
-        secondary: 0xFF7CB4B8,
-        tertiary: 0xFF70F8BA,
-        highlight: 0xFFCAFE48,
-        border: 0xFFFFFFFF,
+    primary: 0xFF57467B,
+    secondary: 0xFF7CB4B8,
+    tertiary: 0xFF70F8BA,
+    highlight: 0xFFCAFE48,
+    border: 0xFFFFFFFF,
 }
 
 export default colors;
